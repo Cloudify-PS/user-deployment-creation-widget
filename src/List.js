2c93f424377276ce03c39c3831e5c0f4970cbf85
Tamer Zorba <abo.al.tot@gmail.com>
2017-11-21 15:41:56 +0200

    redirect on finish, clean code

--- src/List.js
+++ src/List.js
@@ -39,14 +39,22 @@ export default class List extends React.Component {
 
   componentWillMount() {
     let data = this.props.data;
-    this.setState({development: data})
+    this.setState({ development: data })
     this.renderJSON2YAML(data);
-    // data = YAML.parse(data);
-    // this.prepareData(data);
+  }
+
+  renderJSON2YAML(value) {
+    this.setState({ yamlError: null })
+    try {
+      let data = YAML.parse(value || this.state.development);
+      this.prepareData(data);
+    } catch (e) {
+      this.setState({ yamlError: e.message })
+    }
   }
 
   prepareData(data) {
-    
+
     // extract available inputs to the loop
     const visual_data = data.filter(e => [
       'button',
@@ -78,228 +86,224 @@ export default class List extends React.Component {
 
   handleChange = (id, value) => {
     // TODO: before submit check for arrays to join them
-    this.setState({ form: {...this.state.form, [id]: value} });
+    this.setState({ form: Object.assign(this.state.form, { [id]: value }) });
     console.log('form state', this.state.form);
   }
 
-_createDeployment = () => {
-  let errors = {};
-  Object.keys(this.state.form).forEach(item => {
-    // TODO: add more validations methods, by default now is required
-    if (_.isNil(this.state.form[item]) || this.state.form[item] === '') {
-      errors[item] = 'required';
+  _createDeployment = () => {
+    let errors = {};
+    Object.keys(this.state.form).forEach(item => {
+      // TODO: add more validations methods, by default now is required
+      if (_.isNil(this.state.form[item]) || this.state.form[item] === '') {
+        errors[item] = 'required';
+      }
+    });
+    if (Object.keys(errors).length !== 0) {
+      errors.error = 'Please check errors given below';
     }
-  });
-  if (Object.keys(errors).length !== 0) {
-    errors.error = 'Please check errors given below';
-  }
-  this.setState({ errors });
-  if (Object.keys(errors).length === 0) {
+    this.setState({ errors });
+    if (Object.keys(errors).length === 0) {
 
-    // let selectedDeployment = toolbox.getContext().getValue('deploymentId');
-    // var blueprintId = toolbox.getContext().getValue('blueprintId');
+      // let selectedDeployment = toolbox.getContext().getValue('deploymentId');
+      // var blueprintId = toolbox.getContext().getValue('blueprintId');
 
-    const blueprint = this.props.widget.configuration.blueprintId;
-    // const deployment = this.props.widget.configuration.deploymentId;
-    const deployment = this.state.deploymentTitle;//new Date().toISOString();
-    const privateResource = false;
-    const skipPluginsValidation = false;
-    const inputs = {
+      const blueprint = this.props.widget.configuration.blueprintId;
+      // const deployment = this.props.widget.configuration.deploymentId;
+      const deployment = this.state.deploymentTitle;//new Date().toISOString();
+      const privateResource = false;
+      const skipPluginsValidation = false;
+      const inputs = {
         'blueprint_id': 'service',
         'deployment_inputs': this.state.form,
         'deployment_id': 'request_to_admin_' + deployment
+      }
+      // const inputs = this.state.form;
+
+      this.setState({ loading: true });
+      var actions = new Stage.Common.BlueprintActions(this.props.toolbox);
+      actions.doDeploy({ id: blueprint }, deployment, inputs, privateResource, skipPluginsValidation)
+        .then((deployment) => {
+          this.setState({ loading: false, errors: {}, success: 'Your request has been submitted and awaiting approval' });
+          const onFinishRedirect = this.props.widget.configuration.onFinishRedirect;
+          const template = this.props.widget.configuration.template;
+          if(onFinishRedirect){
+            this.props.toolbox.drillDown(this.props.widget, template, { deploymentId: deployment.id }, deployment.id);
+          }
+        })
+        .catch((err) => {
+          this.setState({ loading: false, errors: { error: err.message } });
+        });
+    }
+  }
+
+  /*
+  |--------------------------------------------------------------------------
+  | React Renderer
+  |--------------------------------------------------------------------------
+  */
+  render() {
+    if (this.state.loading) {
+      return <Stage.Basic.Loading />;
     }
 
-    this.setState({ loading: true });
-    var actions = new Stage.Common.BlueprintActions(this.props.toolbox);
-    actions.doDeploy({ id: blueprint }, deployment, inputs, privateResource, skipPluginsValidation)
-      .then((/*deployment*/) => {
-        this.setState({ loading: false, errors: {}, success: 'Your request has been submitted and awaiting approval' });
-        // this.props.onHide();
-      })
-      .catch((err) => {
-        this.setState({ loading: false, errors: { error: err.message } });
-      });
-  }
-}
+    const {Button, Input, Checkbox, Dropdown, Message, Card} = Stage.Basic;
 
-renderJSON2YAML(value){
-  this.setState({ yamlError: null })
-  try {
-    let data = YAML.parse(value || this.state.development);
-    this.prepareData(data);
-  } catch(e){
-    this.setState({yamlError: e.message})
-  }
-}
+    /**
+     * charts visual output
+     */
+    const visuals = this.state.visual_data.map((item, idx) => <div className='chart-box' key={idx} style={{ flexBasis: (item.flex || 33) + '%' }}>
 
-/*
-|--------------------------------------------------------------------------
-| React Renderer
-|--------------------------------------------------------------------------
-*/
-render() {
-  if (this.state.loading) {
-    return <Stage.Basic.Loading />;
-  }
+      {item.type === 'button' && <Button
+        fluid={!item.params.circular}
+        color={item.params.color}
+        circular={item.params.circular}
+        icon={item.params.label ? false : item.params.icon}
+        onClick={() => {
+          item.params.action === 'createDeployment' && this._createDeployment.call(this);
+        } }
+        >
+        {item.params.label}
+      </Button>}
 
-  const {Button, Input, Checkbox, Dropdown, Message, Card} = Stage.Basic;
-
-  /**
-   * charts visual output
-   */
-  const visuals = this.state.visual_data.map((item, idx) => <div className='chart-box' key={idx} style={{ flexBasis: (item.flex || 33) + '%' }}>
-
-    {item.type === 'button' && <Button
-      fluid={!item.params.circular}
-      color={item.params.color}
-      circular={item.params.circular}
-      icon={item.params.label ? false : item.params.icon}
-      onClick={() => {
-        item.params.action === 'createDeployment' && this._createDeployment.call(this);
-      } }
-      >
-      {item.params.label}
-    </Button>}
-
-    {item.type === 'input' && <Input
-      fluid
-      type={item.params.isPassword ? 'password' : 'text'}
-      label={item.params.label}
-      placeholder={item.params.placeholder}
-      value={this.state.form[item.id]}
-      onChange={(proxy, options) => this.handleChange.call(this, item.id, options.value)}
-      />}
-
-    {item.type === 'checkbox' && <Checkbox
-      label={item.params.label}
-      checked={this.state.form[item.id]}
-      onChange={(proxy, options) => this.handleChange.call(this, item.id, options.checked)}
-      />}
-
-    {item.type === 'toggle' && <Checkbox
-      toggle
-      label={item.params.label}
-      checked={this.state.form[item.id]}
-      onChange={(proxy, options) => this.handleChange.call(this, item.id, options.checked)}
-      />}
-
-    {item.type === 'radio' && <div>
-      <b>{item.params.label}</b>
-      <br />
-      <br />
-      {item.params.options.map((option, idx) => <div key={idx}>
-        <Checkbox
-          radio
-          name={item.id}
-          label={option.text}
-          value={option.value}
-          checked={this.state.form[item.id] === option.value}
-          onChange={(proxy, options) => this.handleChange.call(this, item.id, options.value)}
-          />
-      </div>)}
-    </div>}
-
-    {item.type === 'dropdown' && <div>
-      <b>{item.params.label}</b>
-      <br />
-      <br />
-      <Dropdown
+      {item.type === 'input' && <Input
         fluid
-        selection
-        multiple={item.params.multiple}
+        type={item.params.isPassword ? 'password' : 'text'}
+        label={item.params.label}
         placeholder={item.params.placeholder}
-        options={item.params.options}
         value={this.state.form[item.id]}
         onChange={(proxy, options) => this.handleChange.call(this, item.id, options.value)}
-        />
-    </div>}
+        />}
 
-    {item.type === 'range' && <div>
-      <b>{item.params.label}</b>
-      <br />
-      <br />
-      <input
-        type="range"
-        step={item.params.step || 1}
-        min={item.params.min || 0}
-        max={item.params.max || 100}
-        value={this.state.form[item.id]}
-        onChange={proxy => this.handleChange.call(this, item.id, proxy.target.value)}
-        style={{ width: '100%' }}
-        />
-    </div>}
+      {item.type === 'checkbox' && <Checkbox
+        label={item.params.label}
+        checked={this.state.form[item.id]}
+        onChange={(proxy, options) => this.handleChange.call(this, item.id, options.checked)}
+        />}
 
-    {item.type === 'message' && <Message
-      color={item.params.color}
-      >{item.params.label}</Message>}
+      {item.type === 'toggle' && <Checkbox
+        toggle
+        label={item.params.label}
+        checked={this.state.form[item.id]}
+        onChange={(proxy, options) => this.handleChange.call(this, item.id, options.checked)}
+        />}
 
-    {item.type === 'selectbox' && <div>
-      <b>{item.params.label}</b>
-      <br /><SelectBox
-        style={item.params.style}
-        template={item.params.template}
-        options={item.params.options}
-        value={this.state.form[item.id]}
-        onChange={value => this.handleChange.call(this, item.id, value)}
-        />
-    </div>}
+      {item.type === 'radio' && <div>
+        <b>{item.params.label}</b>
+        <br />
+        <br />
+        {item.params.options.map((option, idx) => <div key={idx}>
+          <Checkbox
+            radio
+            name={item.id}
+            label={option.text}
+            value={option.value}
+            checked={this.state.form[item.id] === option.value}
+            onChange={(proxy, options) => this.handleChange.call(this, item.id, options.value)}
+            />
+        </div>)}
+      </div>}
 
-    {
-      this.state.errors[item.id] &&
-      this.state.errors[item.id].split(',').includes('required') &&
-      <div className='err'>Please Fill {item.params.label || 'Field'} its Required</div>
-    }
+      {item.type === 'dropdown' && <div>
+        <b>{item.params.label}</b>
+        <br />
+        <br />
+        <Dropdown
+          fluid
+          selection
+          multiple={item.params.multiple}
+          placeholder={item.params.placeholder}
+          options={item.params.options}
+          value={this.state.form[item.id]}
+          onChange={(proxy, options) => this.handleChange.call(this, item.id, options.value)}
+          />
+      </div>}
 
-  </div>);
+      {item.type === 'range' && <div>
+        <b>{item.params.label}</b>
+        <br />
+        <br />
+        <input
+          type="range"
+          step={item.params.step || 1}
+          min={item.params.min || 0}
+          max={item.params.max || 100}
+          value={this.state.form[item.id]}
+          onChange={proxy => this.handleChange.call(this, item.id, proxy.target.value)}
+          style={{ width: '100%' }}
+          />
+      </div>}
 
-  const errorMessage = this.state.errors.error && <Message color="red" onDismiss={() => this.setState({ errors: {...this.state.errors, error: null} })} >
-        {this.state.errors.error}
-      </Message>;
-  const successMessage = this.state.success && <Message color="green" onDismiss={() => this.setState({ success: null })} >
-        {this.state.success}
-      </Message>;
-  const yamlError = this.state.yamlError && <Message color="yellow" onDismiss={() => this.setState({ yamlError: null })} >
+      {item.type === 'message' && <Message
+        color={item.params.color}
+        >{item.params.label}</Message>}
+
+      {item.type === 'selectbox' && <div>
+        <b>{item.params.label}</b>
+        <br /><SelectBox
+          style={item.params.style}
+          template={item.params.template}
+          options={item.params.options}
+          value={this.state.form[item.id]}
+          onChange={value => this.handleChange.call(this, item.id, value)}
+          />
+      </div>}
+
+      {
+        this.state.errors[item.id] &&
+        this.state.errors[item.id].split(',').includes('required') &&
+        <div className='err'>Please Fill {item.params.label || 'Field'}{' '}its Required</div>
+      }
+
+    </div>);
+
+    const errorMessage = this.state.errors.error && (<Message color="red" onDismiss={() => this.setState({ errors: Object.assign(this.state.errors, { error: null }) })} >
+      {this.state.errors.error}
+    </Message>);
+
+    const successMessage = this.state.success && <Message color="green" onDismiss={() => this.setState({ success: null })} >
+      {this.state.success}
+    </Message>;
+    const yamlError = this.state.yamlError && <Message color="yellow" onDismiss={() => this.setState({ yamlError: null })} >
       {this.state.yamlError}
     </Message>;
-      
+
     const allowNameEdit = this.props.widget.configuration.allowNameEdit;
     const devMode = this.props.widget.configuration.devMode;
-    
+
     console.log
-  return <div>
-    {errorMessage}
-    {successMessage}
+    return <div>
+      {errorMessage}
+      {successMessage}
 
-    {devMode && 
-      <Card fluid>
-        <Card.Content header='Development Mode (you can deactivate it from widget settings)' />
-        <Card.Content>
-          {yamlError}
-          <textArea
-            placeholder="YAML Development Mode"
-            value={this.state.development}
-            onChange={(event) => this.setState({ development: event.target.value })}
-            style={{ width: '100%', height: '200px', fontFamily: 'monospace,monospace', fontSize: '1em' }}
-            />
-        </Card.Content>
-        <Card.Content extra>
-          <Button color="blue" onClick={() => this.renderJSON2YAML.call(this)}>Render</Button>
-        </Card.Content>
-      </Card>}
+      {devMode &&
+        <Card fluid>
+          <Card.Content header='Development Mode (you can deactivate it from widget settings)' />
+          <Card.Content>
+            {yamlError}
+            <textArea
+              placeholder="YAML Development Mode"
+              value={this.state.development}
+              onChange={(event) => this.setState({ development: event.target.value })}
+              style={{ width: '100%', height: '200px', fontFamily: 'monospace,monospace', fontSize: '1em' }}
+              />
+          </Card.Content>
+          <Card.Content extra>
+            <Button color="blue" onClick={() => this.renderJSON2YAML.call(this)}>Render</Button>
+          </Card.Content>
+        </Card>}
 
-    {!this.state.success && allowNameEdit && <Input
-    fluid
-    type={'text'}
-    label="Deployment Title"
-    placeholder="Please Enter Your Deployment Title, should be unique name"
-    value={this.state.deploymentTitle}
-    onChange={(proxy, options) => this.setState({deploymentTitle: options.value})}
-    />}
+      {!this.state.success && allowNameEdit && <Input
+        fluid
+        type={'text'}
+        label="Deployment Title"
+        placeholder="Please Enter Your Deployment Title, should be unique name"
+        value={this.state.deploymentTitle}
+        onChange={(proxy, options) => this.setState({ deploymentTitle: options.value })}
+        />}
 
-  <div className='deployment-visual-creation'>
-  {!this.state.success && visuals}
-  </div>
-</div>;
-}
+      <div className='deployment-visual-creation'>
+        {!this.state.success && visuals}
+      </div>
+    </div>;
+  }
 }
--- src/widget.js
+++ src/widget.js
@@ -2,57 +2,55 @@
  * Created by Tamer on 19/10/2017.
  */
 
- import Actions from './Actions'
- import List from './List'
+import Actions from './Actions'
+import List from './List'
 
 Stage.defineWidget({
-    id: 'visual-input',
-    name: 'Visualization Deployment Creation Input',
-    description: 'Visualization Deployment Creation Input Widget',
-    initialWidth: 12,
-    initialHeight: 16,
-    color : 'purple',
-    hasStyle: true,
-    isReact: true,
-    
-    permission: Stage.GenericConfig.CUSTOM_WIDGET_PERMISSIONS.CUSTOM_ALL,
-    categories: [Stage.GenericConfig.CATEGORY.SYSTEM_RESOURCES],
+  id: 'visual-input',
+  name: 'Visualization Deployment Creation Input',
+  description: 'Visualization Deployment Creation Input Widget',
+  initialWidth: 12,
+  initialHeight: 16,
+  color: 'purple',
+  hasStyle: true,
+  isReact: true,
 
-    initialConfiguration: [
-      { id: 'blueprintId', name: 'Blueprint Id', default: 'COV', type: Stage.Basic.GenericField.STRING },      
-      { id: 'deploymentId', name: 'Deployment Id', default: 'cloudify-deployment-visualization-widget-sample', type: Stage.Basic.GenericField.STRING },
-      {id: 'allowNameEdit', name: 'Allow User to change Deployment Name', default: true, type: Stage.Basic.GenericField.BOOLEAN_TYPE},
-      {id: 'devMode', name: 'Enable Developers Mode', default: false, type: Stage.Basic.GenericField.BOOLEAN_TYPE},
-    ],
+  permission: Stage.GenericConfig.CUSTOM_WIDGET_PERMISSIONS.CUSTOM_ALL,
+  categories: [Stage.GenericConfig.CATEGORY.SYSTEM_RESOURCES],
 
-    fetchData (widget, toolbox, params) {
-        let actions = new Actions ({
-          toolbox,
-          ...widget.configuration,
-        });
-        
-        return actions.doGetInputDescriptor ();
-    },
+  initialConfiguration: [
+    { id: 'blueprintId', name: 'Blueprint Id', default: 'COV', type: Stage.Basic.GenericField.STRING },
+    // { id: 'deploymentId', name: 'Deployment Id', default: 'cloudify-deployment-visualization-widget-sample', type: Stage.Basic.GenericField.STRING },
+    { id: 'allowNameEdit', name: 'Allow User to change Deployment Name', default: true, type: Stage.Basic.GenericField.BOOLEAN_TYPE },
 
-    render: function (widget, data, error, toolbox) {
-        
-        if (_.isEmpty (data)) {
-          return <Stage.Basic.Loading />;
-        }
-    
-        let actions = new Actions ({
-          toolbox,
-          ...widget.configuration,
-        });
+    { id: 'onFinishRedirect', name: 'Redirect to Page on finish', default: false, type: Stage.Basic.GenericField.BOOLEAN_TYPE },
+    { id: 'template', name: 'Template to Redirect to, after finish', default: 'deployment', type: Stage.Basic.GenericField.STRING_TYPE },
 
-    
-        return (
-          <List
-            widget={widget}
-            data={data}
-            toolbox={toolbox}
-            actions={actions}
-          />
-        );
-      }
+    { id: 'devMode', name: 'Enable Developers Mode', default: false, type: Stage.Basic.GenericField.BOOLEAN_TYPE },
+  ],
+
+  fetchData(widget, toolbox, params) {
+    let actions = new Actions(Object.assign({ toolbox }, widget.configuration));
+
+    return actions.doGetInputDescriptor();
+  },
+
+  render: function (widget, data, error, toolbox) {
+
+    if (_.isEmpty(data)) {
+      return <Stage.Basic.Loading />;
+    }
+
+    let actions = new Actions(Object.assign({ toolbox }, widget.configuration));
+
+
+    return (
+      <List
+        widget={widget}
+        data={data}
+        toolbox={toolbox}
+        actions={actions}
+        />
+    );
+  }
 });
